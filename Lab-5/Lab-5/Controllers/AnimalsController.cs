0f45f4ef23e2a817ea3907d7d552edf9ebbd2e2b
s27885 <s27885@pjwstk.edu.pl>
2024-04-16 14:31:16 +0200

    Add basic stuff

---
+++ Lab-5/Lab-5/Controllers/AnimalsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lab_5.Controllers;
+
+[ApiController]
+//[Route("api/animals")]
+[Route("api/[controller]")]
+public class AnimalsController : ControllerBase
+{
+
+  [HttpGet]
+  public IActionResult GetAnimals()
+  {
+    return Ok();
+  }
+    
+}
